c2900c35e6f56aad48f48c7e381aedf3dd7400d4
uwx <maxine@cybergal.com>
2023-10-01 14:14:14 -0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $LLVM_VERSION = $args[0]
 $LLVM_REPO_URL = $args[1]
+$LLVM_BUILD_TOOL = $args[2]
 
 if ([string]::IsNullOrEmpty($LLVM_REPO_URL)) {
     $LLVM_REPO_URL = "https://github.com/llvm/llvm-project.git"
@@ -12,23 +13,25 @@ if ([string]::IsNullOrEmpty($LLVM_VERSION)) {
     Write-Output "  llvm-version         The name of a LLVM release branch without the 'release/' prefix"
     Write-Output "  llvm-repository-url  The URL used to clone LLVM sources (default: https://github.com/llvm/llvm-project.git)"
 
-	exit 1
+    exit 1
 }
+
+$PROJECT_LOCATION = C:\llvm-project
 
 # Clone the LLVM project.
 if (-not (Test-Path -Path "llvm-project" -PathType Container)) {
-	git clone "$LLVM_REPO_URL" llvm-project
+    git clone "$LLVM_REPO_URL" "$PROJECT_LOCATION"
 }
 
-Set-Location llvm-project
+Set-Location "$PROJECT_LOCATION"
 git fetch origin
 git checkout "$LLVM_VERSION"
 
 $LlvmPath = $((Resolve-Path ./llvm).Path)
 
 # Create a directory to build the project.
-New-Item -Path "C:\build" -Force -ItemType "directory"
-Set-Location C:\build
+New-Item -Path "build" -Force -ItemType "directory"
+Set-Location build
 
 # Create a directory to receive the complete installation.
 New-Item -Path "install" -Force -ItemType "directory"
@@ -39,9 +42,7 @@ $CMAKE_ARGUMENTS = ""
 # Adjust cross compilation
 $CROSS_COMPILE = ""
 
-# Run `cmake` to configure the project.
-cmake `
-  -G "Visual Studio 17 2022" `
+$SHARED_FLAGS = `
   -DCMAKE_BUILD_TYPE=MinSizeRel `
   -DCMAKE_INSTALL_PREFIX=destdir `
   -DLLVM_ENABLE_PROJECTS="clang;lld;clang-tools-extra;polly" `
@@ -55,18 +56,35 @@ cmake `
   -DLLVM_INCLUDE_UTILS=OFF `
   -DLLVM_OPTIMIZED_TABLEGEN=ON `
   -DLLVM_TARGETS_TO_BUILD="X86;AArch64;RISCV;WebAssembly" `
-  -DCMAKE_C_FLAGS="/GL /O2" `
-  -DCMAKE_CXX_FLAGS="/GL /O2" `
   $CROSS_COMPILE `
-  $CMAKE_ARGUMENTS `
-  "$LlvmPath"
+  $CMAKE_ARGUMENTS
 
-# Showtime!
-cmake --build . --config Release
+if ($LLVM_BUILD_TOOL -eq "vs") {
+    # Run `cmake` to configure the project.
+    cmake `
+      -G "Visual Studio 17 2022" `
+      $SHARED_FLAGS `
+      "$LlvmPath"
+    
+    # Showtime!
+    cmake --build . --config Release
+    
+    # Not using DESTDIR here, quote from
+    # https://cmake.org/cmake/help/latest/envvar/DESTDIR.html
+    # > `DESTDIR` may not be used on Windows because installation prefix
+    # > usually contains a drive letter like in `C:/Program Files` which cannot
+    # > be prepended with some other prefix.
+    cmake --install . --strip --config Release
+} else if ($LLVM_BUILD_TOOL -eq "clang") {
+    $env:PATH = "C:\msys64\mingw64\bin;$env:PATH"
+ 
+    cmake `
+      -G Ninja `
+         $SHARED_FLAGS `
+      "$LlvmPath"
 
-# Not using DESTDIR here, quote from
-# https://cmake.org/cmake/help/latest/envvar/DESTDIR.html
-# > `DESTDIR` may not be used on Windows because installation prefix
-# > usually contains a drive letter like in `C:/Program Files` which cannot
-# > be prepended with some other prefix.
-cmake --install . --strip --config Release
+    # Showtime!
+    cmake --build . --config MinSizeRel
+
+    cmake --install . --strip --config MinSizeRel
+}
